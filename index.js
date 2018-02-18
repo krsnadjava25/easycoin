04ee553a5646a33edeb9bb77e5bc1de5d5ebf6fe
Krisna Fathurahman <krisna.fathurahman@divusi.com>
2018-02-18 17:40:47 +0700

    Block implemented

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('Hello world!');
+var SHA512 = require("crypto-js/sha512");
+class Block {
+  constructor(data, timestamp, previousHash = null) {
+    this.data = data;
+    this.timestamp = timestamp;
+    this.previousHash = previousHash ? previousHash : '';
+    this.hash = this.calculateHash();
+  }
+
+  calculateHash() {
+    return SHA512(JSON.stringify(this.data) + this.timestamp + this.previousHash).toString();
+  }
+}
+
+const block = new Block(
+  {
+    from: 'sender',
+    to: 'receiver',
+    amount: 0
+  },
+  new Date()
+);
+
+console.log(block.hash);
